f38a85b3762dbe73d9229846191db54aa1dd5e51
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-03 04:21:46 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -148,8 +148,8 @@ const Home: NextPage = () => {
                   </div>
                   <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
                     {generatedBios
-                      .substring(generatedBios.indexOf("3") + 1)
-                      .split("3.")
+                      .substring(generatedBios.indexOf("1") + 3)
+                      .split("2.")
                       .map((generatedBio) => {
                         return (
                           <div
