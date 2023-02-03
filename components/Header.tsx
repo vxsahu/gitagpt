469a2fb9202d8ce19dc62aac826212e247a3ddf8
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-03 10:03:15 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
       <Link href="https://www.gitagpt.in/" className="flex space-x-3">
         <Image
           alt="GitaGPT"
-          src="/Gitagpt.svg"
+          src="/gitagpt.svg"
           width={50}
           height={50}
         />
