0390a7c2c2b70d11025db589bd1c448cc03765c9
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-03 09:39:40 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Header() {
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-between border-b bg-gray-50/50 px-10 py-2 text-center text-lg font-medium backdrop-blur-sm"/>
+    <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-between border-b bg-gray-50/50 px-10 py-2 text-center text-lg font-medium backdrop-blur-sm">
       <title>GitaGPT.in</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Unlock the power of AI with GitaGPT.in – 18 Chapters and 700 Verses of Bhagavad Gita."/>
