562e0df48c712d8f49e9699c7081ff62b62132cc
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-03 06:27:06 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,14 +70,14 @@ const Home: NextPage = () => {
     
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Instagram Bio Generator</title>
+        <title>Bhagvat Gita AI</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
         <h1 className="sm:text-4xl text-xl max-w-2xl font-bold text-slate-900">
-          Generate your next Instagram bio in seconds
+          Arjun, what's troubling you, my friend?
         </h1>
         <div className="max-w-xl w-full">
           <div className="flex mt-10 items-center space-x-3">
@@ -102,7 +102,7 @@ const Home: NextPage = () => {
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
             placeholder={
-              "I'm Digital Marketing Expert. Writing about SEO, Web development, AI, and React / Next.js"
+              "Ask to Krishna"
             }
           />
           <div className="flex mb-5 items-center space-x-3">
@@ -118,7 +118,7 @@ const Home: NextPage = () => {
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
               onClick={(e) => generateBio(e)}
             >
-              Generate your bio &rarr;
+              Ask Krishana &rarr;
             </button>
           )}
           {loading && (
