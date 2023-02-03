f70291c635acf400c9e976e24daa047666937f05
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-03 07:33:28 +0530

    Update DropDown.tsx

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -10,21 +10,21 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export type VibeType = "Professional" | "Casual" | "Funny";
+export type languageType = "Professional" | "Casual" | "Funny";
 
 interface DropDownProps {
-  vibe: VibeType;
-  setVibe: (vibe: VibeType) => void;
+  language: languageType;
+  setLanguage: (language: languageType) => void;
 }
 
-let vibes: VibeType[] = ["Professional", "Casual", "Funny"];
+let languages: languageType[] = ["Professional", "Casual", "Funny"];
 
-export default function DropDown({ vibe, setVibe }: DropDownProps) {
+export default function DropDown({ language, setLanguage }: DropDownProps) {
   return (
     <Menu as="div" className="relative block text-left w-full">
       <div>
         <Menu.Button className="inline-flex w-full justify-between items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-black">
-          {vibe}
+          {language}
           <ChevronUpIcon
             className="-mr-1 ml-2 h-5 w-5 ui-open:hidden"
             aria-hidden="true"
@@ -47,22 +47,22 @@ export default function DropDown({ vibe, setVibe }: DropDownProps) {
       >
         <Menu.Items
           className="absolute left-0 z-10 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-          key={vibe}
+          key={language}
         >
           <div className="">
-            {vibes.map((vibeItem) => (
-              <Menu.Item key={vibeItem}>
+            {languages.map((languageItem) => (
+              <Menu.Item key={languageItem}>
                 {({ active }) => (
                   <button
-                    onClick={() => setVibe(vibeItem)}
+                    onClick={() => setLanguage(languageItem)}
                     className={classNames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      vibe === vibeItem ? "bg-gray-200" : "",
+                      language === languageItem ? "bg-gray-200" : "",
                       "px-4 py-2 text-sm w-full text-left flex items-center space-x-2 justify-between"
                     )}
                   >
-                    <span>{vibeItem}</span>
-                    {vibe === vibeItem ? (
+                    <span>{languageItem}</span>
+                    {language === languageItem ? (
                       <CheckIcon className="w-4 h-4 text-bold" />
                     ) : null}
                   </button>
