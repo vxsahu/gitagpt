ddead687d2d17f5ddca57a7b6b47e28cd1cd7d56
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-03 08:51:06 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import Github from "../components/GitHub";
 import Header from "../components/Header";
 import LoadingDots from "../components/LoadingDots";
 import ResizablePanel from "../components/ResizablePanel";
+import logo from './Gitagpt.png';
 
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
