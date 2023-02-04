7768de84b8d7690948745581d73c96d6ae4b050d
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-04 15:26:04 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,17 +3,15 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
+    <header className="flex justify-center items-center w-full pt-5 pb-5 mt-5 mb-5">
       <Link href="https://www.gitagpt.in/" className="flex space-x-3">
         <Image
           alt="GitaGPT"
           src="/gita-gpt.svg"
-          width={50}
-          height={50}
+          width={45}
+          height={45}
         />
-        <h1>
-          GitaGPT.in
-        </h1>
+        <h1 className="class="justify-items-center my-5 font-bold text-xl">GitaGPT</h1>
       </Link>
     </header>
   );
