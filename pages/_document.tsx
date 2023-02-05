7a3f7210c1fba5e6666d937b51564b90817c280e
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-05 10:36:15 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,15 +6,14 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/gita-gpt.svg" />
-          
-         <title>Gita GPT</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Unlock the power of AI with GitaGPT.in – 18 Chapters and 700 Verses of Bhagavad Gita.">
-        <meta property="og:title" content="Gita GPT">
-        <meta property="og:description" content="Unlock the power of AI with GitaGPT.in – 18 Chapters and 700 Verses of Bhagavad Gita.">
-        <meta property="og:url" content="https://www.gitagpt.in/">
-        <meta property="og:type" content="website">
-          <meta name="twitter:card" content="summary_large_image" />
+          <title>Gita GPT</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="description" content="Unlock the power of AI with Gita GPT – 18 Chapters and 700 Verses of Bhagavad Gita.">
+            <meta property="og:title" content="Gita GPT">
+              <meta property="og:description" content="Unlock the power of AI with Gita GPT – 18 Chapters and 700 Verses of Bhagavad Gita.">
+                <meta property="og:url" content="https://www.gitagpt.in/">
+                  <meta property="og:type" content="website">
+                    <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="GitaGPT" />
           <meta property="og:image" content="https://www.gitagpt.in/og-image.png" />
           <meta name="twitter:image" content="https://www.gitagpt.in/og-image.png" />
