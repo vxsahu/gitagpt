55a51ebaa5ea4564f9681964a81c80a0cac7b99b
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-05 10:39:15 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <title>Gita GPT</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <meta name="description" content="Unlock the power of AI with Gita GPT – 18 Chapters and 700 Verses of Bhagavad Gita." />
-            <meta property="og:title" content="Gita GPT">
+            <meta property="og:title" content="Gita GPT" />
               <meta property="og:description" content="Unlock the power of AI with Gita GPT – 18 Chapters and 700 Verses of Bhagavad Gita." />
                 <meta property="og:url" content="https://www.gitagpt.in/" />
                   <meta property="og:type" content="website" />
