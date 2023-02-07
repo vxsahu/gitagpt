8d5dc31d2398bdbc39d8af1ad848979f0723e70e
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-07 06:47:57 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,7 +20,7 @@ export default function Footer() {
           rel="noreferrer"
           className="font-bold hover:underline transition underline-offset-2"
         >
-          Inspired by Bhagavad Gita
+          Vercel Edge Functions.
         </a>
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
