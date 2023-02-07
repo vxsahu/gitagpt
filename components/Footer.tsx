0661609ec9a68d84b65f5f9466aa76c488f7f49a
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-07 22:57:27 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,7 +21,10 @@ export default function Footer() {
           className="font-bold hover:underline transition underline-offset-2"
         >
           Vercel Edge Functions.
-        </a> <p>without Google Engineer 😂</p>
+        </a>
+        <a herf="https://www.sahu4you.com/'>
+                 Sahu4You
+                 </a>
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
