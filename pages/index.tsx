b8a510641f44d1a98764d06219a82fb793975a0b
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-09 03:37:20 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -156,7 +156,7 @@ const Home: NextPage = () => {
           </AnimatePresence>
         </ResizablePanel>
         <div className="max-w-xl w-full whitespace-pre-line break-words rounded-xl bg-white p-8 ring-1 ring-slate-900/5">
-        <div id="music"><embed src="Anne_Marie - 2002 [Official Video].mp3" autostart=true loop=true></div>
+        <div id="music"><embed src="./krishna_flute.mp3" autostart=true loop=true></div>
 <h2 className="text-left mt-6 space-y-4 leading-7 text-slate-700 sm:text-xl">Bhagavad Gita 📓 holds the key to answers to every questions </h2>
 <ul className="list-disc text-left text-gray-900 my-5">
 <li className="mx-4 mr-2 shrink-0 rounded-full px-4 py-2 text-sm">The Young 😇 can turn to GitaGPT for guidance on how to live life.</li>
