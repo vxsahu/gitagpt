e424ce7ca9bc32a8b6072e66ac77819633fbb9bf
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-09 14:00:43 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,10 +21,10 @@ const Home: NextPage = () => {
   
   const prompt =
     language === "Funny"
-      ? ` Act like bhagavad gita and find solution to answer arjuna. ${bio} ${
+      ? `Act like bhagavad gita and answer to arjuna.${bio}${
           bio.slice(-1) === "." ? "" : "."
         }`
-  : `${language} Generate relevent verse in the style of the Bhagavad Gita: ${bio}${
+  : `${language} Generate relevent verse in style of Bhagavad Gita with reference.${bio}${
           bio.slice(-1) === "." ? "" : "."
         }`;
 
