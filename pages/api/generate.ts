2de7cf3be6ef1a3d22abe5396d0f4b7171534765
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-11 13:30:05 +0530

    Update generate.ts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -18,13 +18,13 @@ const handler = async (req: Request): Promise<Response> => {
   }
 
   const payload: OpenAIStreamPayload = {
-    model: "text-davinci-002",
+    model: "text-davinci-003",
     prompt,
     temperature: 0.7,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 200,
+    max_tokens: 100,
     stream: true,
     n: 1,
   };
