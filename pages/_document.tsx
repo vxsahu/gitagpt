acf200224712a3b85a9f8c54969868587ff0e48e
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-12 22:07:26 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,12 +6,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <title>Gita GPT – Bhagavad Gita as Chatbot</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <meta name="description" content="Unlock the power of AI with Gita GPT – 18 Chapters and 700 Verses of Bhagavad Gita." />
+          <meta name="description" content="Unlock the power of AI with Gita GPT – completely inspired by the Bhagavad Gita." />
             <meta property="og:title" content="Gita GPT" />
           <link rel="canonical" href="https://www.gitagpt.in" />
-  <meta name="twitter:site" content="%s" />
+          <meta name="twitter:site" content="%s" />
               <meta property="og:description" content="Unlock the power of AI with Gita GPT – 18 Chapters and 700 Verses of Bhagavad Gita." />
                 <meta property="og:url" content="https://www.gitagpt.in/" />
                   <meta property="og:type" content="website" />
