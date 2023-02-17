8f81b5c12621f5d7f4ea9faecf67bc0462ac6404
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-17 12:26:30 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,22 +3,21 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex bg-white w-full border-b-1 border-gray-900 mx-auto innerbox flex flex-1 w-full flex-col items-center justify-center">
-      <div className="myhead bg-white flex max-w-5xl px-4 max-xl w-full">
-      <div className="gg-head flex col-1 justify-start">
+    <header className="flex bg-white w-full items-center justify-center text-center border-b-1 border-gray-900 mx-auto innerbox flex flex-1 w-full flex-col items-center justify-center">
+      <div className="myhead bg-white flex max-w-5xl px-4 max-xl text-center text-3xl">
+      <div className="gg-head flex align-center text-center text-3xl">
         <Link href="https://www.gitagpt.in/"
-        className="flex items-center">
+        className="my-5 text-slate-800">
           <Image
           alt="GitaGPT"
-          src="/gita-gpt.svg"
-          width={40}
-          height={40}
+          src="/logo.png"
+          width={100}
+          height={22}
         />
-        <h1 className="ml-2 justify-items-center my-5 text-slate-800 font-bold text-xl">Gita GPT</h1>
       </Link>
       </div>
-      <div className="gg-menu flex gg-menu flex items-center justify-end">
-      <Link href="https://twitter.com/Gita_GPT"
+      <div className="gg-menu flex gg-menu flex items-center">
+        <Link href="https://twitter.com/Gita_GPT"
         className="px-2 space-x-3 max-w-5xl align-center innerbox flex">
         <p className="my-2 mr-2 font-light text-base">Twitter</p>
       </Link>
