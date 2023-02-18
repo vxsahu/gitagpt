9a1c69b2794ca838a78706fbe1766e0f62765f34
Vikas Sahu <40103679+snypiie@users.noreply.github.com>
2023-02-18 14:22:05 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,11 +104,13 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-      <main className="max-w-5xl innerbox flex flex-1 w-full flex-col p-4">
+      <main className="max-w-5xl innerbox flex flex-1 w-full flex-col p-4 my-10">
         <div className="max-xl w-full my-10">
+          <p className="text-xl font-bold text-gray-600 mb-5">Shri Krishna, Radhe Radhe 🦚</p>
+          <p className="text-gray-200 mb-5">Gita GPT is an AI chatbot that uses Bhagavad Gita references to answer your questions. It's an open platform, so feel free to ask Krishna anything you want.</p>
         <h2 className="sm:text-2xl text-xl font-bold mb-5">
             What troubles you, my friend?</h2>
-          <div className="inset-y-0 left-0 flex items-center pl-3">
+          <div className="inset-y-0 left-0 flex items-center my-10 pl-3">
           <input value={gita}
   onChange={(e) => setGita(e.target.value)}
   onInput={limitCharacters}
@@ -118,7 +120,7 @@ const Home: NextPage = () => {
       generateGita(e);
     }
   }}
-  className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-gray-500 focus:border-gray-500 mr-2"
+  className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-gray-500 focus:border-gray-500 mr-2"
   placeholder="How can I find inner peace?"
 />
           {!loading && (
@@ -126,7 +128,7 @@ const Home: NextPage = () => {
               className="bg-black rounded-xl text-white font-bold p-4 hover:bg-black/80 w-half flex-none flex"
               onClick={(e) => generateGita(e)}
             >
-              Find wisdom &rarr;
+              Ask Krishna &rarr;
             </button>
           )}
           {loading && (
@@ -198,7 +200,7 @@ Shree Krishna 🦚</h2>
               >
               ❤️ Read more
             </a>
-            <a href="https://twitter.com/intent/tweet?text=Find%20solace%20in%20the%20wisdom%20of%20Shree%20Krishna%20%F0%9F%A6%9A%0A%0ABhagavad%20Gita%20AI%20App%0A%0Ahttps%3A//www.gitagpt.com" target="_blank" className="inline-block rounded-xl border border-gray-300 bg-white px-4 py-2 w-half font-base transition-colors hover:bg-gray-100 my-2.5 hover:opacity-80">
+            <a href="https://twitter.com/intent/tweet?text=Find%20solace%20in%20the%20wisdom%20of%20Shree%20Krishna%20%F0%9F%A6%9A%0A%0ABhagavad%20Gita%20AI%20App%0A%0Ahttps%3A//www.gitagpt.com" target="_blank" className="inline-block rounded-xl border border-gray-300 bg-white px-4 py-2 w-half font-bold transition-colors hover:bg-gray-100 my-2.5 hover:opacity-80">
               <svg className="text-[#1da1f2] w-4 h-4 mr-2 inline-block" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M459.4 151.7c.325 4.548 .325 9.097 .325 13.65 0 138.7-105.6 298.6-298.6 298.6-59.45 0-114.7-17.22-161.1-47.11 8.447 .974 16.57 1.299 25.34 1.299 49.06 0 94.21-16.57 130.3-44.83-46.13-.975-84.79-31.19-98.11-72.77 6.498 .974 12.99 1.624 19.82 1.624 9.421 0 18.84-1.3 27.61-3.573-48.08-9.747-84.14-51.98-84.14-102.1v-1.299c13.97 7.797 30.21 12.67 47.43 13.32-28.26-18.84-46.78-51.01-46.78-87.39 0-19.49 5.197-37.36 14.29-52.95 51.65 63.67 129.3 105.3 216.4 109.8-1.624-7.797-2.599-15.92-2.599-24.04 0-57.83 46.78-104.9 104.9-104.9 30.21 0 57.5 12.67 76.67 33.14 23.72-4.548 46.46-13.32 66.6-25.34-7.798 24.37-24.37 44.83-46.13 57.83 21.12-2.273 41.58-8.122 60.43-16.24-14.29 20.79-32.16 39.31-52.63 54.25z"></path></svg>
               Share on Twitter
             </a>
@@ -211,7 +213,7 @@ Shree Krishna 🦚</h2>
         <p className="mb-2.5 space-y-4 text-slate-800 py-2">
               Bhagavad Gita holds the key to unlocking answers to every query and challenges. Ask anything like any miracle, powerful mantras that help in real life.</p>
               <a
-              className="inline-block flex-warp w-half rounded-xl mr-2 border border-gray-300 bg-white px-4 py-2 text-slate-700 transition-colors hover:bg-gray-100 my-5"
+              className="inline-block font-bold flex-warp w-half rounded-xl mr-2 border border-gray-300 bg-white px-4 py-2 text-slate-700 transition-colors hover:bg-gray-100 my-5"
               href="https://www.sahu4you.com/gita-gpt/" target="_blank" rel="noopener noreferrer"
               >
               Start Learning 📔
